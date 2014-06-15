56af4a225ed5856cd13e2a6aab512981f9581faf
seanerchen <seanerchen@gmail.com>
2014-06-15 15:39:06 +0800

    合并排序

--- sort/mergesort.c
+++ sort/mergesort.c
@@ -1,62 +1,94 @@
-/*
- * 合并排序
- */
-
 #include <stdio.h>
+#include <limits.h>
+#include <string.h>
 #include "lib/swapnum.h"
 #include "lib/rangerand.h"
 #include "lib/putarr.h"
 #define SIZE 10
 
-void merge(int *arr, int start, int mid, int end);
-//void mergesort(int arr, int size);
+void merge(int *arr,int low,int mid,int high);
+void merge2(int *arr,int low,int mid,int high);
+void mergeSort(int *arr, int low, int high);
 
 int main()
 {
-    //int arr[SIZE];
-    //rangeRand(0, 50, SIZE, arr);
-    int arr[SIZE] = {1, 3, 5, 7,  2, 4, 6, 8, 9, 10 };
+    int arr[SIZE];
+    rangeRand(0, 50, SIZE, arr);
+    //int arr[SIZE] = {1, 3, 5, 7,  2, 4, 6, 8, 9, 10 };
     putarr(arr, SIZE);
-    merge(arr, 0, 3, 9);
+    mergeSort(arr, 0, SIZE-1);
+    //merge(arr, 0, 0, 0);
     putarr(arr, SIZE);
 }
 
-void merge(int *arr, int start, int mid, int end)
-{
-    int size1 = mid - start + 1; 
-    int size2 = end - mid;
-    int size = end - start + 1;
-    int *arrtemp1 = malloc(size1*sizeof(int));
-    int *arrtemp2 = malloc(size2*sizeof(int));
-    int i, j, k;
-    for(i = 0; i  < size1; i++) {
-        arrtemp1[i] = arr[start + i];
+void mergeSort(int *arr, int low, int high) {
+    if(low < high) {
+        int mid = (int)(low+high)/2;
+        mergeSort(arr, low, mid);
+        mergeSort(arr,  mid + 1, high);
+        merge2(arr, low, mid, high);
+
     }
-    for(i = 0; i  < size2; i++) {
-        arrtemp2[i] = arr[mid + i + 1];
-    }
-    i = 0; 
-    j = 0; 
-    int l = 0; 
-    int n = size1 > size2 ? size2 : size1;
-    for(k = 0; k < n; k++) {
-        if(arrtemp1[i] <= arrtemp2[j]) {
-            arr[l++] = arrtemp1[i++];
+}
+
+void merge(int *arr, int low, int mid, int high) {
+    int *temp = malloc((high - low + 1)*sizeof(int));
+    int m = low;
+    int n = mid + 1;
+    int k = 0;
+
+    while(m <= mid && n <= high) {
+        if(arr[m] <= arr[n]) {
+            temp[k++] = arr[m++];
         } else {
-            arr[l++] = arrtemp2[j++];
+            temp[k++] = arr[n++];
         }
     }
 
-    while( n < size1) {
-            arr[l++] = arrtemp1[i++];
-            n++;
-    } 
-    while( n < size2) {
-            arr[l++] = arrtemp2[j++];
-            n++;
-    } 
+    if(m > mid) {
+        while(n <= high) {
+            temp[k++] = arr[n++];
+        }
+    }
 
-    free(arrtemp1);
-    free(arrtemp2);
+    if(n > high) {
+        while( m <= mid) {
+            temp[k++] = arr[m++];
+        }
+
+    }
+    for(int i = low; i <= high; i++) {
+        arr[i] = temp[i-low];
+    }
+    free(temp);
 }
 
+void merge2(int *arr, int low, int mid, int high) {
+    int l1 = mid - low + 1;
+    int l2 = high - mid;
+    int i;
+    int m = 0;
+    int n = 0;
+    int *temp1 = malloc((l1+1)*sizeof(int));
+    int *temp2 = malloc((l2+1)*sizeof(int));
+
+    for(i = 0; i < l1; i++) {
+        temp1[i] = arr[low + i];
+    }
+    temp1[l1] = INT_MAX;
+    for(i = 0; i < l2; i++) {
+        temp2[i] = arr[mid+ i + 1];
+    }
+    temp2[l2] = INT_MAX;
+
+    for(i = low; i <= high; i++){
+        if(temp1[m] < temp2[n])
+            arr[i] = temp1[m++];
+        else
+            arr[i] = temp2[n++];
+    }
+
+    free(temp1);
+    free(temp2);
+
+}
