bf1a82ff93b17329f06a6a82f21e2859d3bdbfe5
seanerchen <seanerchen@gmail.com>
2014-06-16 21:43:57 +0800

    添加冒泡排序算法

--- sort/buddle.c
+++ sort/buddle.c
@@ -1,42 +1,37 @@
+
+/*
+ * 冒泡排序法 
+ * author: sean chen
+ */
 #include <stdio.h>
-#include "lib/exchange.h"
-#include "lib/draw.h"
-#include "lib/common.h"
+#include "lib/swapnum.h"
+#include "lib/rangerand.h"
+#include "lib/putarr.h"
 
 #define SIZE 10
-void buddle(int *arr, int size);
+
+void bubble(int *arr, int size);
 
 int main()
 {
     int i;
-    int arr[SIZE] = {};
-    srand(time(NULL));
-    for(i = 0; i  < SIZE; i++)
-    {
-        arr[i] = 10 + rand()% 20;
-    }
-    for(i = 0; i  < SIZE; i++)
-    {
-        mvprintw(5, 5, "%d\t", arr[i]);
-    }
-    initDraw();
-    buddle(arr, SIZE);
-    endDraw();
-    putchar('\n');
+    int arr[SIZE];
+    rangeRand(0, 50, 10, arr);
+    putarr(arr, SIZE);
+    bubble(arr, SIZE);
+    putarr(arr, SIZE);
     return 0;
 }
 
 
-void buddle(int *arr, int size)
+void bubble(int *arr, int size)
 {
     int i, j;
     for(i = 0; i <  size; i++){
         for(j = size - 1; j > i; j--) {
             if(arr[j] < arr[j-1]) {
-                exchange(&arr[j], &arr[j-1]);
-                doDraw(arr, size, j);
+                swapNum(&arr[j], &arr[j-1]);
             }
-            sleep(1000000);
         }
     }
 }
